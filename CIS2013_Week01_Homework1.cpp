2d4443a926722ede93c78f8d5bcfb40a1e8e3031
Natasha S Love <debora@inbox.ru>
2018-01-23 20:48:59 -0600

    Initial commit

---
+++ CIS2013_Week01_Homework1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+string something;
+cout << "\n";
+cout << "==========================================================================\n"; 
+cout << "Natasha Love\n";
+cout << "\n";
+cout << "Student, CIS, OSU - OKC\n";
+cout << "900 N. Portland Ave.				p: (405) 641-3181\n"; 
+cout << "Oklahoma City, OK				e: debora@inbox.ru\n"; 
+cout << "						g: github.com/Natashalove\n"; 
+cout << "==========================================================================\n";
+cout << "\n";
+return 0;
+}
---
+++ CIS2013_Week01_Homework1.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+string something;
+cout << "===================================================="; 
+cout << "Natasha Love" ;
+cout << "Student, CIS, OSU - OKC" ;
+cout << "900 N. Portland Ave.				p: (405) 641-3181"; 
+cout << "Oklahoma City, OK					e: debora@inbox.ru"; 
+cout << "									g: github.com/Natashalove"; 
+cout << "====================================================";
+return 0;
+}
